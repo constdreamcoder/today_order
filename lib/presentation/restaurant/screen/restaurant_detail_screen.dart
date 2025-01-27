474e5ddb7ae709ebf79080a9d4626d2bfbf95437
constdreamcoder <scdreamricher@gmail.com>
2025-01-28 01:30:01 +0900

    Feat: 레스토랑 상세 기본 화면 UI 구현

--- lib/presentation/restaurant/screen/restaurant_detail_screen.dart
+++ lib/presentation/restaurant/screen/restaurant_detail_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:today_order/core/common/layout/default_layout.dart';
 import 'package:today_order/presentation/restaurant/component/restaurant_card.dart';
 
+import '../../product/component/product_card.dart';
+import '../../rating/component/rating_card.dart';
+
 class RestaurantDetailScreen extends StatelessWidget {
   final String id;
 
@@ -12,5 +15,79 @@ class RestaurantDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultLayout(
+      title: '불타타는 떡볶이',
+      body: CustomScrollView(
+        slivers: [
+          renderTop(),
+          renderLabel(),
+          renderProducts(),
+          renderRatings(),
+        ],
+      ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        isFromDetail: true,
+      ),
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return const SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderProducts() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          childCount: 10,
+          (context, index) {
+            return InkWell(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: ProductCard(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderRatings() {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          childCount: 10,
+          (context, index) {
+            return InkWell(
+              onTap: (){},
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: RatingCard(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
 }
