d83077568ce88a34a768465d2728b6ecd13d5323
constdreamcoder <scdreamricher@gmail.com>
2025-01-30 14:10:06 +0900

    Feat: Dio Client, Flutter Secure Storage DI 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:today_order/core/common/component/custom_text_form_field.dart';
 import 'package:today_order/core/routing/routes.dart';
 import 'package:today_order/core/theme/app_colors.dart';
 import 'package:today_order/presentation/root_tab.dart';
 import 'package:today_order/presentation/user/screen/login_screen.dart';
 
+import 'core/di/di_setup.dart';
 import 'core/routing/provider/router_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  diSetup();
   await dotenv.load(fileName: ".env");
   runApp(
     const ProviderScope(
