5fe15cec8f277318bd376078c1a36ca8e5c688b0
constdreamcoder <scdreamricher@gmail.com>
2025-01-31 23:29:23 +0900

    Feat: Restaurant Repository에 레스토랑 상세 기능 추가

--- lib/domain/respository/restaurant_repository.dart
+++ lib/domain/respository/restaurant_repository.dart
@@ -1,5 +1,11 @@
+import 'package:today_order/domain/model/restaurant_detail_model.dart';
 import 'package:today_order/domain/model/restaurant_model.dart';
 
 import '../../core/respository/base_pagination_repository.dart';
 
-abstract interface class RestaurantRepository implements IBasePaginationRepository<RestaurantModel> {}
+abstract interface class RestaurantRepository
+    implements IBasePaginationRepository<RestaurantModel> {
+  Future<RestaurantDetailModel> getDetail({
+    required String rid,
+  });
+}
