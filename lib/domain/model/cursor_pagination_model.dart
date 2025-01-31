f5c74b7543954e8ed02264303365fa33c918d862
constdreamcoder <scdreamricher@gmail.com>
2025-01-31 09:46:03 +0900

    Feat: Cursor Pagination 모델 구현

---
+++ lib/domain/model/cursor_pagination_model.dart
@@ -0,0 +1,75 @@
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'cursor_pagination_model.g.dart';
+
+abstract class CursorPaginationBase {}
+
+class CursorPaginationError extends CursorPaginationBase {
+  final String message;
+
+  CursorPaginationError({required this.message});
+}
+
+class CursorPaginationLoading extends CursorPaginationBase {}
+
+
+@JsonSerializable(
+  genericArgumentFactories: true,
+)
+class CursorPagination<T> extends CursorPaginationBase {
+  final CursorPaginationMeta meta;
+  final List<T> data;
+
+  CursorPagination({
+    required this.meta,
+    required this.data,
+  });
+
+  CursorPagination copyWith({
+    CursorPaginationMeta? meta,
+    List<T>? data,
+  }) {
+    return CursorPagination(
+      meta: meta ?? this.meta,
+      data: data ?? this.data,
+    );
+  }
+
+  factory CursorPagination.fromJson(Map<String, dynamic> json, T Function(Object? json) fromJsonT) {
+    return _$CursorPaginationFromJson(json, fromJsonT);
+  }
+}
+
+@JsonSerializable()
+class CursorPaginationMeta {
+  final int count;
+  final bool hasMore;
+
+  CursorPaginationMeta({
+    required this.count,
+    required this.hasMore,
+  });
+
+  factory CursorPaginationMeta.fromJson(Map<String, dynamic> json) {
+    return _$CursorPaginationMetaFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() => _$CursorPaginationMetaToJson(this);
+}
+
+// 새로고침
+class CursorPaginationRefetching<T> extends CursorPagination<T> {
+  CursorPaginationRefetching({
+    required super.meta,
+    required super.data,
+  });
+}
+
+// 목록 맨 아래로 내려서 fetching
+class CursorPaginationFetchingMore<T> extends CursorPagination<T> {
+  CursorPaginationFetchingMore({
+    required super.meta,
+    required super.data,
+  });
+}
