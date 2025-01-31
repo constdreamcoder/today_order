2ab44b4ed699dd8b4aca13d222c4ff01cfaab62c
constdreamcoder <scdreamricher@gmail.com>
2025-01-31 11:49:57 +0900

    Feat: 식당 Data Source 정의

---
+++ lib/data/data_source/remote/restaurant_api.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:retrofit/retrofit.dart';
+import 'package:today_order/core/model/cursor_pagination_model.dart';
+import 'package:today_order/domain/model/login_response.dart';
+import 'package:today_order/domain/model/product_model.dart';
+import 'package:today_order/domain/model/restaurant_model.dart';
+
+import '../../../core/model/pagination_params.dart';
+
+part 'restaurant_api.g.dart';
+
+@RestApi()
+abstract class RestaurantApi {
+  factory RestaurantApi(Dio dio, {String baseUrl}) = _RestaurantApi;
+
+  @GET('/product')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<CursorPagination<RestaurantModel>> paginate({
+    @Queries() PaginationParams? paginationParams = const PaginationParams(),
+  });
+}
