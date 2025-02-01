931249cd1cf3889f729a2d16a575d0999a61b939
constdreamcoder <scdreamricher@gmail.com>
2025-02-01 20:28:43 +0900

    Feat: RestaurantRatingRepository 정의

---
+++ lib/domain/respository/restaurant_rating_repository.dart
@@ -0,0 +1,6 @@
+import 'package:today_order/domain/model/rating_model.dart';
+
+import '../../core/respository/base_pagination_repository.dart';
+
+abstract interface class RestaurantRatingRepository
+    implements IBasePaginationRepository<RatingModel> {}
