915ef047ad01e99e408d247f78fa09da86952e05
constdreamcoder <scdreamricher@gmail.com>
2025-02-02 17:21:12 +0900

    Feat: 장바구니 업데이트 기능 Shopping Cart Repository에 추가

--- lib/domain/respository/shopping_cart_repository.dart
+++ lib/domain/respository/shopping_cart_repository.dart
@@ -2,4 +2,6 @@ import 'package:today_order/domain/model/shopping_cart_item_model.dart';
 
 abstract interface class ShoppingCartRepository {
   Future<List<ShoppingCartItemModel>> getShoppingCart();
+
+  Future<List<ShoppingCartItemModel>> patchShoppingCart();
 }
